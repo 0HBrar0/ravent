ef69f43deff681810ba882fca3c18457f63210cf
0hbrar0 <hbrar2004@gmail.com>
2026-03-22 09:03:57 -0700

    time is gonna be doubled, txt was updated

--- test.py
+++ test.py
@@ -4,5 +4,6 @@ a = 0
 
 while True:
     time.sleep(a)
+    time.sleep(a)
     a += 1
-    print(f"Next Message will print in {a} seconds.")
+    print(f"Next Message will print in double of {a} seconds.")
